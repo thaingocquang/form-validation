343493cec688d8d993c667e0c655d4adafb8298f
thaingocquang <thaingocquang147@gmail.com>
2021-09-09 23:27:54 +0700

    complete isRequired and isEmail

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
 function Validator(options) {
 
+    function validate(inputElement, rule) {
+        var errorMessage = rule.test(inputElement.value);
+        var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+
+        if(errorMessage) {
+            errorElement.innerText = errorMessage;
+            inputElement.parentElement.classList.add('invalid');
+        } else {
+            errorElement.innerText = '';
+            inputElement.parentElement.classList.remove('invalid');
+        }
+    }
+
+    // Lấy element của form cần validate
     var formElement = document.querySelector(options.form);
 
     if(formElement) {
         
         options.rules.forEach(function(rule) {
-            console.log(rule);
+            var inputElement = formElement.querySelector(rule.selector);
+
+            if(inputElement) {
+                // Xử lý trường hợp blur khỏi input
+                inputElement.onblur = function() {
+                    validate(inputElement, rule);
+                }
+
+                // Xử lý mỗi khi người dùng nhập vào input
+                inputElement.oninput = function() {
+                    var errorElement = inputElement.parentElement.querySelector('.form-message');
+                    errorElement.innerText ='';
+                    inputElement.parentElement.classList.remove('invalid');
+                }
+            }
+
         });
 
     }
@@ -17,8 +46,8 @@ function Validator(options) {
 Validator.isRequired = function(selector) {
     return {
         selector: selector,
-        test: function() {
-
+        test: function(value) {
+            return value.trim() ? undefined : 'Vui lòng nhập trường này';
         }
     };
 }
@@ -26,8 +55,9 @@ Validator.isRequired = function(selector) {
 Validator.isEmail = function(selector) {
     return {
         selector: selector,
-        test: function() {
-
+        test: function(value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regex.test(value) ? undefined : 'Trường này phải là email';
         }
     };
 }
